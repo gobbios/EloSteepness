52485c7567c3badefac47aef91edd02ef2c0f941
Christof Neumann <christofneumann1@gmail.com>
2021-08-03 11:23:21 +0200

    numeric steepness summaries (#1)

---
+++ R/steepness_precis.R
@@ -0,0 +1,49 @@
+#' numeric summary of steepness
+#'
+#' @param x result from \code{\link{elo_steepness_from_matrix}} or
+#'        \code{\link{davids_steepness}}
+#' @param quantiles numeric, the quantiles to be returned
+#'
+#' @return a data.frame with one row
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' res <- elo_steepness_from_matrix(dommats$elephants, n_rand = 10,
+#'                                  cores = 4,
+#'                                  iter = 3000, warmup = 2000, refresh = 0)
+#' steepness_precis(res)
+#' }
+
+steepness_precis <- function(x, quantiles = c(0.045, 0.25, 0.75, 0.955)) {
+  if (!"steepness" %in% names(x)) {
+    stop("wrong input object")
+  }
+  is_elo <- "cumwinprobs" %in% names(x)
+  x <- x$steepness
+  
+  
+  out <- data.frame(mean = mean(x),
+                    sd = sd(x),
+                    median = median(x),
+                    mad = mad(x))
+  nm <- paste0("q", substr(sprintf("%.3f", quantiles), 3, 5))
+  for (i in seq_len(length(quantiles))) {
+    out <- cbind(out, quantile(x, prob = quantiles[i]))
+    colnames(out)[ncol(out)] <- nm[i]
+  }
+  
+  if (is_elo) {
+    out$mean_cv <- NA
+    out$median_cv <- NA
+    if (ncol(x) > 1) {
+      means <- apply(x, 2, mean)
+      out$mean_cv[1] <- sd(means) / mean(means)
+      medians <- apply(x, 2, median)
+      out$median_cv[1] <- sd(medians) / mean(medians)
+    }
+  }
+  
+  rownames(out) <- NULL
+  out
+}
