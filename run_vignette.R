189f1d0c1e68353595a0e89cb1bf2266f321b45d
Christof Neumann <christofneumann1@gmail.com>
2022-01-24 22:55:53 +0100

    dealing with vignette cntd

--- run_vignette.R
+++ run_vignette.R
@@ -1,9 +1,23 @@
 # thank you: https://www.kloppenborg.ca/2021/06/long-running-vignettes/
+# which seems to work for html, but not for pdf...
+# old_wd <- getwd()
+# 
+# setwd("vignettes/")
+# knitr::knit("tutorial.Rmd.original", output = "tutorial.Rmd")
+# knitr::purl("tutorial.Rmd.original", output = "tutorial.R")
+# 
+# setwd(old_wd)
 
+
+
+# my attempt, even more hackish...
 old_wd <- getwd()
 
 setwd("vignettes/")
-knitr::knit("tutorial.Rmd.original", output = "tutorial.Rmd")
-knitr::purl("tutorial.Rmd.original", output = "tutorial.R")
+file.copy(from = "tutorial.Rmd.original", to = "tutorial.Rmd")
+rmarkdown::render(input = "tutorial.Rmd" ) # , output_file ="tutorial.pdf", output_format = "pdf_document",
+file.rename(from = "tutorial.pdf", to = "pdf_files/tutorial.pdf")
+file.remove("tutorial.Rmd")
+file.remove(list.files("tutorial_files", recursive = TRUE, full.names = TRUE))
 
 setwd(old_wd)
