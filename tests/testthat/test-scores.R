c01c8d4929cc280a96da72aa10d3ed6bdb2d11fc
Christof Neumann <christofneumann1@gmail.com>
2022-10-11 22:38:40 +0200

    fix bug in tests

--- tests/testthat/test-scores.R
+++ tests/testthat/test-scores.R
@@ -1,11 +1,13 @@
 
-data("dommats", package = "EloRating")
+n <- sample(6:8, 1)
+mat <- matrix(ncol = n, nrow = n, 0)
+colnames(mat) <- rownames(mat) <- letters[1:n]
+mat[upper.tri(mat)] <- sample(2:3, sum(upper.tri(mat)), TRUE)
 
-mat <- dommats$elephants
-res1 <- scores(davids_steepness(mat, refresh = 0, iter = 1000))
+res1 <- scores(davids_steepness(mat, refresh = 0, chains = 2, iter = 1000))
 xord <- sample(seq_len(ncol(mat)))
 mat <- mat[xord, xord]
-res2 <- scores(davids_steepness(mat, refresh = 0, iter = 1000))
+res2 <- scores(davids_steepness(mat, refresh = 0, chains = 2, iter = 1000))
 
 res1 <- res1[order(res1$mean), ]
 res2 <- res2[order(res2$mean), ]
@@ -14,9 +16,9 @@ test_that("order in results is independent of order in input matrix", {
   expect_true(all(res1$id == res2$id))
 })
 
-m <- dommats$elephants
-res1 <- elo_steepness_from_matrix(m, algo = "fixed_sd", n_rand = 1, refresh = 0, iter = 1000)
-res2 <- elo_steepness_from_matrix(m, algo = "fixed_sd", n_rand = 2, refresh = 0, iter = 1000)
+
+res1 <- elo_steepness_from_matrix(mat, algo = "fixed_sd", n_rand = 1, refresh = 0, chains = 2, iter = 1000)
+res2 <- elo_steepness_from_matrix(mat, algo = "fixed_sd", n_rand = 2, refresh = 0, chains = 2, iter = 1000)
 
 res1 <- scores(res1)
 res2 <- scores(res2)
